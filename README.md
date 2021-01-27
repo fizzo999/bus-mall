115b83eb86d30c242f58d5f9d1001c1084e71eda
FIZZO PANNOSCH <fizzo999@gmail.com>
2021-01-26 23:12:37 -0800

    chart, image reiteration check, turn off event listener, implement prompt how many rounds you want to look at images.

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # bus-mall
 lab11 Codefellows 201 create a digital product test to evaluate consumer preferences
 
+## update 01/27/2021
+
+
 ## update 01/26/2021
 I woke up at 5.15am with an inspiration on how to display the results of the survey INCLUDING the images. Yesterday I had only compiled the results into a list `<li>` - I thought about adding them as bullet-images in CSS. I couldn`t figure that out but it led me to go back to basics. Just looking over my code on how we had displayed the random images in class. And then translate that to the results images. And it worked.
 
 Then I continued on to implement the solution I observed TA Chance Harmon give to Matt Simms yesterday. Displaying content with the CSS property of display: none. And then changing that dynamically in JS through a button. define a variable that grabs onto the `id=buttonShow` and then `buttonShow.addEventListener` on the button. And in the function simply set the `buttonShow.style.display='block'`
 
 I LOVE THAT. Having a new idea and following it. Until it all works out. Beautiful.
+
+afternoon/evening: I added the chart with CDN chart, from class this morning, just had to push the image names into an array so it would be an array.
+Then I added another button and styled it and implemented - either display the image-list - OR do the chart. Yay.
+Then I added Tedd Knechts math check. Makeing sure that the 3 images are unique and new. Difficult task since I had to break up my math function. Got stuck and asked TA Nicco for help. Sout out to Nicco. What I learned - if you return sth from a function that is an array - you ll have to put that return value into square brackets !!!
+Then I had to do it again to split a function - or better to say wrap the eventListener on the images into a named function so I could removeEventListener. - And it worked.
+Then I made the li list elements display into 2 columns.
+Finally I tried to implement a customer input for deciding how many rounds they want to look at images. Still working on.
+Update - I just made it work. Had another typo in the ID to grab onto. Fixed that, made the counter work correctly, made the button disappear when done.... YAY.
--- app.js
+++ app.js
@@ -31,127 +31,291 @@ new Product('img/unicorn.jpg', 'unicorn');
 new Product('img/usb.gif', 'usb');
 new Product('img/water-can.jpg', 'water-can');
 new Product('img/wine-glass.jpg', 'wine-glass');
-console.log(Product.allImages);
+// console.log(Product.allImages);
 // grab the HTML elements and put them in 1 + 3 variables
 var productContainer = document.getElementById('productImagesContainer');
 var leftImage = document.getElementById('leftImage');
 var middleImage = document.getElementById('middleImage');
 var rightImage = document.getElementById('rightImage');
 var resultsButton = document.getElementById('showResultsBtn');
-var resultsContainer = document.getElementById('resultsContainer')
+var chartButton = document.getElementById('showChartBtn');
+var resultsContainer = document.getElementById('resultsContainer');
+var chartDisplay1 = document.getElementById('chartDisplay1');
+var roundsCounterElement = document.getElementById('roundsCounter');
+var sectionHeader = document.getElementById('sectionHeader');
+var numberOfClicks = 0;
+var rounds = 4;
+var roundsInput = 0;
+var lastImageIndexNumArray = [];
+var tempImageIndexNumArray = [];
+
+// var roundsInput = 0;
+function askRounds() {
+  roundsInput = prompt('Please select how many rounds of ProductSelection you would like to have displayed. Pick a number between 1 and 25');
+  while ((roundsInput < 1) || (roundsInput > 25)) {
+    roundsInput = prompt('Please only enter numbers from 1 to 25 !');
+  }
+  rounds = roundsInput - 1;
+
+  // console.log(roundsInput);
+  // roundsCounterElement.textContent = (roundsInput - 1) + ' rounds left';
+  return rounds, roundsInput;
+}
+
+askRounds();
+
+function genRandImage() {
+
+  tempImageIndexNumArray[0] = genRanNum();
+  var leftProduct = Product.allImages[tempImageIndexNumArray[0]];
+
+  Product.allImages[tempImageIndexNumArray[0]].timesShown++;
+
+  tempImageIndexNumArray[1] = genRanNum();
+  var middleProduct = Product.allImages[tempImageIndexNumArray[1]];
+
+  Product.allImages[tempImageIndexNumArray[1]].timesShown++;
+
+  tempImageIndexNumArray[2] = genRanNum();
+  var rightProduct = Product.allImages[tempImageIndexNumArray[2]];
+  Product.allImages[tempImageIndexNumArray[2]].timesShown++;
+
+  for (var i = 0; i < 3; i++) {
+    lastImageIndexNumArray[i] = tempImageIndexNumArray[i];
+  }
+
+  return [leftProduct, middleProduct, rightProduct];
+
+}
+
+function genRanNum() {
+
+  var randNumber = Math.floor(Math.random() * Product.allImages.length);
+
+  while (lastImageIndexNumArray.includes(randNumber) || tempImageIndexNumArray.includes(randNumber)) {
+    randNumber = Math.floor(Math.random() * Product.allImages.length);
+  }
+  return (randNumber);
+}
 
 // generate random number between 1 and the length of the array that contains all objects (=allImages)
 
-function genRandProduct() {
-  var leftIndex = Math.floor(Math.random() * Product.allImages.length);
-  var middleIndex = Math.floor(Math.random() * Product.allImages.length);
-  // make sure the 2nd number doesn match the first
-  while (middleIndex === leftIndex) {
-    middleIndex = Math.floor(Math.random() * Product.allImages.length)
-  }
-  // make sure the 3rd number doesnt match the first or second
-  var rightIndex = Math.floor(Math.random() * Product.allImages.length);
-  while ((rightIndex === leftIndex) || (rightIndex === middleIndex)) {
-    rightIndex = Math.floor(Math.random() * Product.allImages.length)
-  }
-
-  var leftProduct = Product.allImages[leftIndex];
-  Product.allImages[leftIndex].timesShown++;
-  // console.log('times Shown just increased to: ' + Product.allImages[leftIndex].timesShown);
-  var middleProduct = Product.allImages[middleIndex];
-  Product.allImages[middleIndex].timesShown++;
-
-  var rightProduct = Product.allImages[rightIndex];
-  Product.allImages[rightIndex].timesShown++;
-
-
-  return [leftProduct, middleProduct, rightProduct];
-}
-
 function renderProduct(leftProduct, middleProduct, rightProduct) {
-  // leftProduct.setAttribute('src', leftProduct.image);
+
   leftImage.src = leftProduct.imagePath;
-  leftImage.setAttribute('data-id', leftProduct.imagePath);
-
   middleImage.src = middleProduct.imagePath;
-  middleImage.setAttribute('data-id', middleProduct.imagePath);
-
   rightImage.src = rightProduct.imagePath;
-  rightImage.setAttribute('data-id', rightProduct.imagePath);
+  roundsCounterElement.textContent = (roundsInput) + ' rounds left';
+
 }
 
-var randProd = genRandProduct();
+var randProd = genRandImage();
 renderProduct(randProd[0], randProd[1], randProd[2]);
-var numberOfClicks = 0;
-var rounds = 5;
 
-// everytime an image was clicked
-productContainer.addEventListener('click', function (event) {
-  event.preventDefault();
-  // console.log(event.target);
-  // console.log(event.target.src);
+function handleImageClick(event) {
+  roundsCounterElement.textContent = roundsInput + ' rounds left';
+  roundsInput--;
+
   for (var i = 0; i < Product.allImages.length; i++) {
     if (event.target.src.includes(Product.allImages[i].imagePath)) {
       Product.allImages[i].timesClicked++;
     }
   }
   if (numberOfClicks < rounds) {
-    var newProduct = genRandProduct();
+    var newProduct = genRandImage();
     renderProduct(newProduct[0], newProduct[1], newProduct[2]);
     numberOfClicks++;
-    console.log('the number of clicks ' + numberOfClicks);
+    // console.log('the number of clicks ' + numberOfClicks);
   } else {
-    console.log('total has reached ' + numberOfClicks);
+    // console.log('total has reached ' + numberOfClicks);
     leftImage.src = '';
     middleImage.src = '';
     rightImage.src = '';
+    productContainer.removeEventListener('click', handleImageClick);
+    // console.log('HAS IT STOPPED ALLOWING TO CLICK ????, ', event.target);
     return checkForResults();
   }
-})
-// productContainer.removeEventListener('click', function (event) { });
+}
+// everytime an image was clicked
+productContainer.addEventListener('click', handleImageClick);
+
 function checkForResults() {
   for (var k = 0; k < Product.allImages.length; k++) {
     if (Product.allImages[k].timesClicked > 0) {
       hasBeenClickedArray.push(Product.allImages[k].imageName);
     }
   }
-  console.log('HAS BEEN CLICKED YAYAYAY', hasBeenClickedArray);
+  // console.log('HAS BEEN CLICKED YAYAYAY', hasBeenClickedArray);
   printResults();
-
+  printChart();
 }
 
 resultsButton.addEventListener('click', function (event) {
   event.preventDefault();
   resultsContainer.style.display = 'block';
-
-
-
+  chartDisplay1.style.display = 'none';
 })
 
-
-
-
-
-
+chartButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  resultsContainer.style.display = 'none';
+  chartDisplay1.style.display = 'block';
+})
 
 function printResults() {
-
+  sectionHeader.style.display = 'none';
   resultsContainer.style.display = 'none';
+  roundsCounterElement.style.display = 'none';
   resultsButton.style.display = 'block';
+  chartButton.style.display = 'block';
 
   var ulElement = document.getElementById('results');
 
   for (var l = 0; l < Product.allImages.length; l++) {
 
-    var liElement = document.createElement('li');
     var imgResults = document.createElement('img');
+    var liElement = document.createElement('li');
     // liElement.textContent = 'I love you, Fizzo';
-
-    liElement.textContent = `${Product.allImages[l].imageName} had ${Product.allImages[l].timesClicked} votes, and was viewed ${Product.allImages[l].timesShown} times`;
-    ulElement.appendChild(liElement);
     imgResults.src = Product.allImages[l].imagePath;
     ulElement.appendChild(imgResults);
 
+    liElement.textContent = `${Product.allImages[l].imageName} had ${Product.allImages[l].timesClicked} votes, and was viewed ${Product.allImages[l].timesShown} times `;
+    ulElement.appendChild(liElement);
+    resultsContainer.appendChild(ulElement);
   }
 }
+
+// this grabs our canvas element and select a context called ('2d;);
+//    this operation enables us to draw 2 dimensional shapes use the chartContext variable
+var chartContext = document.getElementById('myChart').getContext('2d');
+
+var votesByProduct = [];
+var timesProductsAreShown = [];
+var productsNamesArray = [];
+
+// what is this for loop doing?
+//  Is it required?
+function printChart() {
+  for (var i = 0; i < Product.allImages.length; i++) {
+    votesByProduct.push(Product.allImages[i].timesClicked);
+    timesProductsAreShown.push(Product.allImages[i].timesShown);
+    productsNamesArray.push(Product.allImages[i].imageName);
+  }
+  // console.log('votesByProductASrray Values are : ', votesByProduct);
+  // This is an object constructor, from chart.js.  Because we have installed our chart.js file from the cdn, we should have access to a new constructor
+  var myChart = new Chart(chartContext, {
+    type: 'bar',
+    data: {
+      labels: productsNamesArray, // array of strings goes here
+      datasets: [{
+        label: 'times clicked',
+        // data: [15, 50, 3, 5, 2, 3], // array of numbers goes here
+        data: votesByProduct,
+        backgroundColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)'
+        ],
+        borderWidth: 1
+      },
+      {
+        label: 'times displayed',
+        data: timesProductsAreShown,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+};
 
